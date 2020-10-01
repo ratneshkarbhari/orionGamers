9f958e8b8e1965292d8da52cfa46f08aa7146f8d
Ratnesh Karbhari <ratnesh@pop-os.localdomain>
2020-10-01 11:41:02 +0530

    How it works page modified

--- application/views/site_pages/how_it_works.php
+++ application/views/site_pages/how_it_works.php
@@ -5,14 +5,14 @@
         </div>
         <div class="login-section section " style="margin-bottom: 3%;">
             <!-- <h1 class="page-title">How it Works?</h1> -->
-            <p class="description">Welcome, gamers our website is made for the most overpowered gamers to have all the privileges of the games we are offering. We sell all the Premium packs, Diamonds, UC, CP, Gems of your favorite games. We provide it in a very reasonable and affordable price. To receive this discount there are certain criteria’s to be followed.</p>
-            <p class='description'>First you need to register/login in your account from our website..</p>
+            <p style="margin: 2% 0;" class="description">Welcome, gamers our website is made for the most overpowered gamers to have all the privileges of the games we are offering. We sell all the Premium packs, Diamonds, UC, CP, Gems of your favorite games. We provide it in a very reasonable and affordable price. To receive this discount there are certain criteria’s to be followed.</p>
+            <p style="margin: 2% 0;" class='description'>First you need to register/login in your account from our website..</p>
             <img src="<?php echo site_url('assets/images/ss1.png'); ?>" style="width: 100%;">
-            <p class="description">Select the product and click on buy now proceed to check out</p>
+            <p style="margin: 2% 0;" class="description">Select the product and click on buy now proceed to check out</p>
             <img src="<?php echo site_url('assets/images/ss2.png'); ?>" style="width: 100%;">
-            <p class="description">Get done with your payment..</p>
+            <p style="margin: 2% 0;" class="description">Get done with your payment..</p>
             <img src="<?php echo site_url('assets/images/ss3.png'); ?>" style="width: 100%;">
-            <p class="description">After payment you will receive your unique referral code. You have to share and join your friends using your referral code minimum 3 friends/people are required to get your discount. Don’t worry your money is safe with us. We have come with this affiliate policy just to give you the product in less price. You will able to see in your website account who you have referred will show in red and people who have joined will show in green once 3 green is done. A forum will unlock submit your registered/ working g-mail id. You will receive the google play voucher in your G-mail .Then Enjoy and avail your product in your specific game….</p>
+            <p style="margin: 2% 0;" class="description">After payment you will receive your unique referral code. You have to share and join your friends using your referral code minimum 3 friends/people are required to get your discount. Don’t worry your money is safe with us. We have come with this affiliate policy just to give you the product in less price. You will able to see in your website account who you have referred will show in red and people who have joined will show in green once 3 green is done. A forum will unlock submit your registered/ working g-mail id. You will receive the google play voucher in your G-mail .Then Enjoy and avail your product in your specific game….</p>
             <img src="<?php echo site_url('assets/images/ss4.png'); ?>" style="width: 100%;">
         </div>
     </div>
