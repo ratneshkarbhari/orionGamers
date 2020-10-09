85fe1ef112baf8cceb16d80cc2dee11f13d2c775
Ratnesh <rkarbhari23@gmail.com>
2020-10-09 11:29:35 +0530

    updated the from for this email sending

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -250,7 +250,7 @@
             $headers  = 'MIME-Version: 1.0' . "\r\n";
             $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
             
-            $from = "email_verification@origamers.com,Email Verification";
+            $from = "email_verification@origamers.com";
 
             // Create email headers
             $headers .= 'From: '.$from."\r\n".
