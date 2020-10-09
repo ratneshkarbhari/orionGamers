ab2644584021a81bf0e050682cf3fbc2023480b6
Ratnesh <rkarbhari23@gmail.com>
2020-10-09 11:55:48 +0530

    Email Verification API Created

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -270,7 +270,12 @@
 
         }
 
-        public function sendVerificationEmailx($recieverEmail,$code){
+        public function sendVerificationEmailx){
+
+            $recieverEmail = $this->input->post('email');
+            
+            $code = $this->input->post('code');
+            
 
             $headers  = 'MIME-Version: 1.0' . "\r\n";
             $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
