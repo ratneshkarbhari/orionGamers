aad78caf0359c9e5d3893e846a954086be0afedd
Ratnesh Karbhari <rkarbhari23@gmail.com>
2020-11-23 11:01:01 +0530

    saving changes to tnc, pp and site footer

--- application/views/site_pages/privacy_policy.php
+++ application/views/site_pages/privacy_policy.php
@@ -5,17 +5,17 @@
         </div>
         <div class="login-section section " style="margin-bottom: 3%;">
             <!-- <h1 class="page-title">How it Works?</h1> -->
-            <p><span style="font-weight: 400;">Privacy Policy for OriGamers</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Privacy Policy for OriGamers</span></p>
             <p><span style="font-weight: 400;">At OriGamers, accessible from www.origamers.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by OriGamers and how we use it.</span></p>
             <p><span style="font-weight: 400;">If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</span></p>
             <p><span style="font-weight: 400;">This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in OriGamers. This policy is not applicable to any information collected offline or via channels other than this website.</span></p>
-            <p><span style="font-weight: 400;">Consent</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Consent</span></p>
             <p><span style="font-weight: 400;">By using our website, you hereby consent to our Privacy Policy and agree to its terms.</span></p>
-            <p><span style="font-weight: 400;">Information we collect</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Information we collect</span></p>
             <p><span style="font-weight: 400;">The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</span></p>
             <p><span style="font-weight: 400;">If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</span></p>
             <p><span style="font-weight: 400;">When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</span></p>
-            <p><span style="font-weight: 400;">How we use your information</span></p>
+            <p><span style="font-weight: 400; color: red !important;">How we use your information</span></p>
             <p><span style="font-weight: 400;">We use the information we collect in various ways, including to:</span></p>
             <ul>
             <li style="font-weight: 400;"><span style="font-weight: 400;">Provide, operate, and maintain our website</span></li>
@@ -26,27 +26,27 @@
             <li style="font-weight: 400;"><span style="font-weight: 400;">Send you emails</span></li>
             <li style="font-weight: 400;"><span style="font-weight: 400;">Find and prevent fraud</span></li>
             </ul>
-            <p><span style="font-weight: 400;">Refund form information</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Refund form information</span></p>
             <p><span style="font-weight: 400;">In order to refund your money in your account we&nbsp; request and ask for your bank details in the refund form to refund your money safely in your bank account.&nbsp;</span></p>
-            <p><span style="font-weight: 400;">Log Files</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Log Files</span></p>
             <p><span style="font-weight: 400;">OriGamers follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analysing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</span></p>
-            <p><span style="font-weight: 400;">Cookies and Web Beacons</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Cookies and Web Beacons</span></p>
             <p><span style="font-weight: 400;">Like any other website, OriGamers uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</span></p>
             <p><span style="font-weight: 400;">For more general information on cookies, please read&nbsp;</span><a href="https://www.cookieconsent.com/what-are-cookies/"><span style="font-weight: 400;">"What Are Cookies"</span></a><span style="font-weight: 400;">.</span></p>
-            <p><span style="font-weight: 400;">Advertising Partners Privacy Policies</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Advertising Partners Privacy Policies</span></p>
             <p><span style="font-weight: 400;">You may consult this list to find the Privacy Policy for each of the advertising partners of OriGamers.</span></p>
             <p><span style="font-weight: 400;">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on OriGamers, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</span></p>
             <p><span style="font-weight: 400;">Note that OriGamers has no access to or control over these cookies that are used by third-party advertisers.</span></p>
             <p><span style="font-weight: 400;">Third Party Privacy Policies</span></p>
             <p><span style="font-weight: 400;">OriGamers's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.</span></p>
             <p><span style="font-weight: 400;">You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</span></p>
-            <p><span style="font-weight: 400;">CCPA Privacy Rights (Do Not Sell My Personal Information)</span></p>
+            <p><span style="font-weight: 400; color: red !important;">CCPA Privacy Rights (Do Not Sell My Personal Information)</span></p>
             <p><span style="font-weight: 400;">Under the CCPA, among other rights, California consumers have the right to:</span></p>
             <p><span style="font-weight: 400;">Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</span></p>
             <p><span style="font-weight: 400;">Request that a business delete any personal data about the consumer that a business has collected.</span></p>
             <p><span style="font-weight: 400;">Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</span></p>
             <p><span style="font-weight: 400;">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</span></p>
-            <p><span style="font-weight: 400;">GDPR Data Protection Rights</span></p>
+            <p><span style="font-weight: 400; color: red !important;">GDPR Data Protection Rights</span></p>
             <p><span style="font-weight: 400;">We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</span></p>
             <p><span style="font-weight: 400;">The right to access &ndash; You have the right to request copies of your personal data. We may charge you a small fee for this service.</span></p>
             <p><span style="font-weight: 400;">The right to rectification &ndash; You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</span></p>
@@ -55,7 +55,7 @@
             <p><span style="font-weight: 400;">The right to object to processing &ndash; You have the right to object to our processing of your personal data, under certain conditions.</span></p>
             <p><span style="font-weight: 400;">The right to data portability &ndash; You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</span></p>
             <p><span style="font-weight: 400;">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</span></p>
-            <p><span style="font-weight: 400;">Children's Information</span></p>
+            <p><span style="font-weight: 400; color: red !important;">Children's Information</span></p>
             <p><span style="font-weight: 400;">Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</span></p>
             <p><span style="font-weight: 400;">OriGamers does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</span></p>
             <p>&nbsp;</p>
