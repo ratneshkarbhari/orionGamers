b141f09894f74e3741b691b39f13c2b7768b6824
Ratnesh <rkarbhari23@gmail.com>
2020-12-24 11:26:09 +0530

    Pay now page done fixing page load

--- application/views/site_pages/buy_now.php
+++ application/views/site_pages/buy_now.php
@@ -37,37 +37,9 @@ function getCallbackUrl()
 }
 
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>PayUmoney BOLT PHP7 Kit</title>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
-<!-- this meta viewport is required for BOLT //-->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" >
-<!-- BOLT Sandbox/test //-->
-<!-- <script id="bolt" src="https://sboxcheckout-static.citruspay.com/bolt/run/bolt.min.js" bolt-
-color="e34524" bolt-logo="http://boltiswatching.com/wp-content/uploads/2015/09/Bolt-Logo-e14421724859591.png"></script> -->
-<!-- BOLT Production/Live //-->
 <script id="bolt" src="https://checkout-static.citruspay.com/bolt/run/bolt.min.js" bolt-color="000000" bolt-logo="<?php echo site_url('assets/images/origamersTransparent.png'); ?>"></script>
 
 </head>
-<style type="text/css">
-	.main {
-		margin-left:30px;
-		font-family:Verdana, Geneva, sans-serif, serif;
-	}
-	.text {
-		float:left;
-		width:180px;
-	}
-	.dv {
-		margin-bottom:5px;
-	}
-</style>
 <body>
 <?php print_r($gameProductData); ?>
 <div class="main">
