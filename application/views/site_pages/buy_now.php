e7f444400ef4c6354847e97f758c482d8cddaa5d
Ratnesh <rkarbhari23@gmail.com>
2020-12-24 11:34:03 +0530

    Pay now page done fixing page load

--- application/views/site_pages/buy_now.php
+++ application/views/site_pages/buy_now.php
@@ -1,8 +1,6 @@
 <main class="page-content" id="buy-now">
-<script id="bolt" src="https://checkout-static.citruspay.com/bolt/run/bolt.min.js" bolt-color="e34524" bolt-logo="<?php echo site_url('assets/images/origamersTransparent.png'); ?>"></script>
 
   <div class="container">
-  
     <div id="top-header-my-account row card" style="padding: 5% 0; background-color: white;margin: 5% 0;">
       <h1 style="background-color: white !important; color: black !important;" class="text-center"><?php echo $title; ?></h1>
     </div>
@@ -14,158 +12,22 @@
         <div class="card-body">
           <h3 class="card-title" style="background-color: white !important; color: black !important;"><?php echo $gameProductData['title']; ?></h3>
           <p class="card-text"><?php echo $gameProductData['description']; ?></p>
-          <?php
-
-if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0){
-	//Request hash
-	$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';	
-	if(strcasecmp($contentType, 'application/json') == 0){
-		$data = json_decode(file_get_contents('php://input'));
-		$hash=hash('sha512', $data->key.'|'.$data->txnid.'|'.$data->amount.'|'.$data->pinfo.'|'.$data->fname.'|'.$data->email.'|||||'.$data->udf5.'||||||'.$data->salt);
-		$json=array();
-		$json['success'] = $hash;
-    	echo json_encode($json);
-	
-	}
-	exit(0);
-}
- 
-function getCallbackUrl()
-{
-	$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-	return $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . 'response.php';
-}
-
-?>
-
-<?php print_r($gameProductData); ?>
-<div class="main">
-	<form action="#" id="payment_form">
-    <input type="hidden" id="udf5" name="udf5" value="BOLT_KIT_PHP7" />
-    <input type="hidden" id="surl" name="surl" value="<?php echo getCallbackUrl(); ?>" />
-    <div class="dv">
-    <span class="text"><label>Merchant Key:</label></span>
-    <span><input type="hidden" id="key" name="key" placeholder="Merchant Key" value="lomegegA" /></span>
-    </div>
-    
-    <div class="dv">
-    <span class="text"><label>Merchant Salt:</label></span>
-    <span><input type="hidden" id="salt" name="salt" placeholder="Merchant Salt" value="X971ICRWjz" /></span>
-    </div>
-    
-    <div class="dv">
-    <span class="text"><label>Transaction/Order ID:</label></span>
-    <span><input type="hidden" id="hidden" name="txnid" placeholder="Transaction ID" value="<?php echo  "Txn" . rand(10000,99999999)?>" /></span>
-    </div>
-    
-    <div class="dv">
-    <span class="text"><label>Amount:</label></span>
-    <span><input type="hidden" id="amount" name="amount" placeholder="Amount" value="<?php echo $game_product_data["sale_price"]; ?>" /></span>    
-    </div>
-    
-    <div class="dv">
-    <span class="text"><label>Product Info:</label></span>
-    <span><input type="hidden" id="pinfo" name="pinfo" placeholder="Product Info" value="<?php echo $game_product_data["description"]; ?>" /></span>
-    </div>
-    
-    <div class="dv">
-    <span class="text"><label>First Name:</label></span>
-    <span><input type="hidden" id="fname" name="fname" placeholder="First Name" value="<?php echo $_SESSION["first_name"]; ?>" /></span>
-    </div>
-    
-    <div class="dv">
-    <span class="text"><label>Email ID:</label></span>
-    <span><input type="hidden" id="email" name="email" placeholder="Email ID" value="<?php echo $_SESSION["email"]; ?>" /></span>
-    </div>
-    
-   
-    <div class="dv">
-    <span class="text"><label>Hash:</label></span>
-    <span><input type="hidden" id="hash" name="hash" placeholder="Hash" value="" /></span>
-    </div>
-    
-    
-    <div><input type="submit" class="btn btn-danger" style="background-color: red;" value="Pay" onclick="launchBOLT(); return false;" /></div>
-	</form>
-</div>
-<script type="text/javascript"><!--
-$('#payment_form').bind('keyup blur', function(){
-	$.ajax({
-          url: 'index.php',
-          type: 'post',
-          data: JSON.stringify({ 
-            key: $('#key').val(),
-			salt: $('#salt').val(),
-			txnid: $('#txnid').val(),
-			amount: $('#amount').val(),
-		    pinfo: $('#pinfo').val(),
-            fname: $('#fname').val(),
-			email: $('#email').val(),
-			mobile: $('#mobile').val(),
-			udf5: $('#udf5').val()
-          }),
-		  contentType: "application/json",
-          dataType: 'json',
-          success: function(json) {
-            if (json['error']) {
-			 $('#alertinfo').html('<i class="fa fa-info-circle"></i>'+json['error']);
-            }
-			else if (json['success']) {	
-				$('#hash').val(json['success']);
-            }
-          }
-        }); 
-});
-//-->
-</script>
-<script type="text/javascript"><!--
-function launchBOLT()
-{
-	bolt.launch({
-	key: $('#key').val(),
-	txnid: $('#txnid').val(), 
-	hash: $('#hash').val(),
-	amount: $('#amount').val(),
-	firstname: $('#fname').val(),
-	email: $('#email').val(),
-	phone: $('#mobile').val(),
-	productinfo: $('#pinfo').val(),
-	udf5: $('#udf5').val(),
-	surl : $('#surl').val(),
-	furl: $('#surl').val(),
-	mode: 'dropout'	
-},{ responseHandler: function(BOLT){
-	console.log( BOLT.response.txnStatus );		
-	if(BOLT.response.txnStatus != 'CANCEL')
-	{
-		//Salt is passd here for demo purpose only. For practical use keep salt at server side only.
-		var fr = '<form action=\"'+$('#surl').val()+'\" method=\"post\">' +
-		'<input type=\"hidden\" name=\"key\" value=\"'+BOLT.response.key+'\" />' +
-		'<input type=\"hidden\" name=\"salt\" value=\"'+$('#salt').val()+'\" />' +
-		'<input type=\"hidden\" name=\"txnid\" value=\"'+BOLT.response.txnid+'\" />' +
-		'<input type=\"hidden\" name=\"amount\" value=\"'+BOLT.response.amount+'\" />' +
-		'<input type=\"hidden\" name=\"productinfo\" value=\"'+BOLT.response.productinfo+'\" />' +
-		'<input type=\"hidden\" name=\"firstname\" value=\"'+BOLT.response.firstname+'\" />' +
-		'<input type=\"hidden\" name=\"email\" value=\"'+BOLT.response.email+'\" />' +
-		'<input type=\"hidden\" name=\"udf5\" value=\"'+BOLT.response.udf5+'\" />' +
-		'<input type=\"hidden\" name=\"mihpayid\" value=\"'+BOLT.response.mihpayid+'\" />' +
-		'<input type=\"hidden\" name=\"status\" value=\"'+BOLT.response.status+'\" />' +
-		'<input type=\"hidden\" name=\"hash\" value=\"'+BOLT.response.hash+'\" />' +
-		'</form>';
-		var form = jQuery(fr);
-		jQuery('body').append(form);								
-		form.submit();
-	}
-},
-	catchException: function(BOLT){
- 		alert( BOLT.message );
-	}
-});
-}
-//--
-</script>	
+          <form id="redirectForm" method="post" action="https://www.cashfree.com/checkout/post/submit">
+            <input type="hidden" name="appId" value="33090190a25fd481164ee1c1c09033"/>
+            <input type="hidden" name="orderId" value="<?php echo $orderData['id']; ?>"/>
+            <input type="hidden" name="orderAmount" value="<?php echo $orderData['amount']; ?>"/>
+            <input type="hidden" name="orderCurrency" value="INR"/>
+            <input type="hidden" name="orderNote" value=""/>
+            <input type="hidden"  name="customerName" value="<?php echo $_SESSION['first_name'].' '.$_SESSION['last_name']; ?>"/>
+            <input type="hidden" name="customerEmail" value="<?php echo $_SESSION['email']; ?>"/>
+            <input type="hidden" class="form-control" name="customerPhone" value="<?php echo $orderData['customerPhone']; ?>"/>
+            <br>
+            <input type="hidden" name="returnUrl" value="<?php echo $orderData['returnUrl']; ?>"/>
+            <input type="hidden" name="signature" value="<?php echo $token; ?>"/>
 
 
+            <button type="submit" class="btn btn-large btn-block btn-danger">Pay Now</button>
+          </form>
         </div>
       </div>
       <div class="col-lg-4 col-md-12 col-sm-12"></div>
