acf143b903d68050a438595382af6172b9fe377d
Ratnesh <you@example.com>
2021-01-22 11:45:42 +0530

    Force accepting tnc

--- application/views/site_pages/buy_now.php
+++ application/views/site_pages/buy_now.php
@@ -25,7 +25,25 @@
 			  <input type="hidden" name="furl" value="<?php echo site_url(''); ?>">
 			  <input type="hidden" name="hash" value="<?php $hashSequence2 = 'lomegegA|'.$txnid.'|'.$gameProductData["sale_price"].'|'.$gameProductData["description"].'|'.$_SESSION["first_name"].'|'.$_SESSION["email"].'|||||||||||X971ICRWjz';echo $hash = strtolower(hash("sha512",$hashSequence2)); ?>">
 			  <input type="hidden" name="service_provider" value="payu_paisa">
-			  <button class="btn btn-danger" style="background-color: red !important; color: white !important;" type="submit">Pay ₹ <?php echo $gameProductData["sale_price"]; ?></button>
+
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="tnc_cb">
+          <label class="form-check-label" for="tnc_cb">
+            I Agree to all Terms and Conditions
+          </label>
+        </div>
+        <p>Read the <a href="<?php echo site_url("terms-and-conditions"); ?>">Terms and Conditions</a></p>
+        <script>
+          $("input#tnc_cb").click(function (e) { 
+            e.preventDefault();
+            if($(this).prop(":checked")){
+              $("button#PayButton").css("display","block");
+            }else{
+              $("button#PayButton").css("display","none");
+            }
+          });
+        </script>
+			  <button id="PayButton" class="btn btn-danger d-none" style="background-color: red !important; color: white !important;" type="submit">Pay ₹ <?php echo $gameProductData["sale_price"]; ?></button>
 		  </form>
         </div>
       </div>
