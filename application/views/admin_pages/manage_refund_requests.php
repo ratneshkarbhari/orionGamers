14a3015fdd5ec04c9eab95c83387f65586412d11
Ratnesh <ratneshkarbhari23@gmail.com>
2021-02-22 16:59:51 +0530

    saving changes

--- application/views/admin_pages/manage_refund_requests.php
+++ application/views/admin_pages/manage_refund_requests.php
@@ -44,7 +44,7 @@ echo $title; ?></h2>
                         <?php echo $refund_request['email']; ?>
                     </td>
                     <td>
-                        <?php $bankDetails = json_decode($refund_request['bank_details'],TRUE); 
+                        <?php $bankDetails = json_decode($refund_request['bank_details']); 
                             echo $bankDetails["account_number"].'<br>'.$bankDetails['ifsc'].'<br>'.$bankDetails["bank_name"].'<br>'.$bankDetails["branch_name"];
                         ?>
                     </td>
